6287889f462db81aca4d8c0d62460d6bab632563
Keyvan <k.kholghi@gmail.com>
2019-10-18 07:56:32 +0000

    Modified intended query

--- 1_queries/3_properties_by_city.sql
+++ 1_queries/3_properties_by_city.sql
@@ -1,4 +1,4 @@
-SELECT properties.*, avg(property_reviews.rating) as average_rating
+SELECT properties, avg(property_reviews.rating) as average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_id
 WHERE city LIKE '%ancouv%'
